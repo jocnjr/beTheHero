755b0908416cbceb344f659ae2bb2868acc481f8
daxoliveira <dax-oliveira-br@gmail.com>
2020-03-25 21:54:06 -0300

    complete backend and basic frontend

---
+++ backend/src/controllers/ProfileController.js
@@ -0,0 +1,11 @@
+module.exports = {
+  async index(request, response) {
+    const ngo_id = request.headers.authorization
+
+    const incidents = await RTCPeerConnection('incidents')
+      .where('ngo_id', ngo_id)
+      .select('*');
+
+    return response.json(incidents);
+  }
+}
